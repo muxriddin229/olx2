a7d7974d129235bd14972b0197629ce50f971f30
muxriddin229 <mavlonovm229@gmail.com>
2025-03-20 16:13:58 +0500

    ggg

--- config/db.js
+++ config/db.js
@@ -3,16 +3,17 @@ const { Sequelize } = require("sequelize");
 const db = new Sequelize("n17", "root", "2004", {
   host: "localhost",
   dialect: "mysql",
+  logging: false,
 });
 
 async function connectDb() {
   try {
     await db.authenticate();
-    console.log("Databasega muvaffaqiyatli ulandi.");
-    await db.sync({ force: true });
-    console.log("Model sinxronizatsiya qilindi.");
+    console.log("✅ Databasega muvaffaqiyatli ulandi.");
+    await db.sync({ alter: true });
+    console.log("✅ Model sinxronizatsiya qilindi.");
   } catch (error) {
-    console.error("DB ulanish xatosi:", error);
+    console.error("❌ DB ulanish xatosi:", error);
   }
 }
 
