499f2c3e55a91e08e7c38cda368df24171953a8e
abdulhadiy-1 <hadiyhadiy2008@gmail.com>
2025-03-20 16:37:15 +0500

    ss

--- routes/category.js
+++ routes/category.js
@@ -2,6 +2,25 @@ const { Router } = require("express");
 const Category = require("../model/category");
 const { Op } = require("sequelize");
 const joi = require("joi");
+const winston = require("winston");
+require("winston-mongodb")
+
+const { json, combine, timestamp } = winston.format;
+
+const logger = winston.createLogger({
+  level: "silly",
+  format: combine(timestamp(), json()),
+  transports: [
+    new winston.transports.File({  filename: "loglar" }),
+    new winston.transports.Console(),
+    new winston.transports.MongoDB({
+      collection: "loglars",
+      db: "mongodb://localhost:27017/nt"
+    }),
+  ],
+});
+
+const routerLogger = logger.child({module: "categories"})
 
 const route = Router();
 
@@ -42,12 +61,14 @@ route.get("/", async (req, res) => {
       where.name = { [Op.startsWith]: name };
     }
     let categories = await Category.findAll({ where, limit, offset });
-    if (!categories.length)
+    if (!categories.length) 
       return res.status(404).json({ message: "no categories found" });
     res.json(categories);
+    routerLogger.log("info", "category get")
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "server error" });
+    routerLogger.log("error", "error on category get")
   }
 });
 
@@ -77,9 +98,12 @@ route.get("/:id", async (req, res) => {
     if (!category)
       return res.status(404).json({ message: "category not found" });
     res.json(category);
+    routerLogger.log("info", "category get by id")
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "server error" });
+    routerLogger.log("error", "error on category get by id")
+
   }
 });
 
@@ -118,9 +142,13 @@ route.post("/", async (req, res) => {
     if (error) return res.status(400).json({ message: error.details[0].message });
     await Category.create({ name, image });
     res.json({ message: "category created" });
+    routerLogger.log("info", "category created")
+
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "server error" });
+    routerLogger.log("error", "error on category update")
+
   }
 });
 
@@ -170,9 +198,13 @@ route.patch("/:id", async (req, res) => {
     if (error) return res.status(400).json({ message: error.details[0].message });
     await category.update({ name, image });
     res.json({ message: "category updated" });
+    routerLogger.log("info", "category updated")
+
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "server error" });
+    routerLogger.log("error", "error on category patch")
+
   }
 });
 
@@ -202,9 +234,13 @@ route.delete("/:id", async (req, res) => {
     if (!category) return res.status(404).json({ message: "category not found" });
     await category.destroy();
     res.json({ message: "category deleted" });
+    routerLogger.log("info", "category deleted")
+
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "server error" });
+    routerLogger.log("error", "error on category delete")
+
   }
 });
 
